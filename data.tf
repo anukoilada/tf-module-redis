a8b1f909884ad131c82599cb219824d4833d7bb7
anupama <anupama@gmail.com>
2023-05-01 23:08:15 -0400

    Git Auto Push

---
+++ data.tf
@@ -0,0 +1,17 @@
+# Retrieves the information from the remote state file
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+  config = {
+    bucket = "b53-tfstatebucket"
+    key    = "vpc/${var.ENV}/terraform.tfstate"
+    region = "us-east-1"
+   }
+}
+
+data "aws_secretsmanager_secret" "secrets" {
+   name   =  "roboshop/secrets"
+}
+
+data "aws_secretsmanager_secret_version" "secrets" {
+  secret_id     = data.aws_secretsmanager_secret.secrets.id
+}
